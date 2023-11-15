e86f105083618eea5bb4789a5a3a222dc0c75a25
Tori <toriroettger@gmail.com>
2023-11-14 21:09:58 -0600

    added limiter

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const { PORT = 3001 } = process.env;
 const cors = require("cors");
 const errorHandler = require("./middleware/error-handler");
 const { requestLogger, errorLogger } = require("./middleware/logger");
+const { limiter } = require("./utils/limiter");
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
@@ -40,6 +41,7 @@ const routes = require("./routes");
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
+app.use(limiter);
 app.use(requestLogger);
 app.use(routes);
 app.use(errorLogger);
